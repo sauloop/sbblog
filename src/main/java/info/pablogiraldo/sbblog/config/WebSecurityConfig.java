d53f64f12aad55ca716ef951104129384e2abf72
Pablo Giraldo <pablogirald@yahoo.es>
2020-06-16 12:29:23 +0200

    Add https

--- src/main/java/info/pablogiraldo/sbblog/config/WebSecurityConfig.java
+++ src/main/java/info/pablogiraldo/sbblog/config/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/", "/trueknic", "/formsearch", "/search").permitAll().anyRequest().authenticated().and()
 				.formLogin().loginPage("/login").permitAll().and().logout().permitAll();
 
-//		http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
+		http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
 	}
 
 	@Override
